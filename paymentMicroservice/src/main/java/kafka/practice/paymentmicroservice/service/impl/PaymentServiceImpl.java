3877cc55548eddb9e3538f11ede07710cc36d3a5
Frizzer <eo.ursuz@gmail.com>
2022-10-18 18:24:11 +0300

    Final commit v2

---
+++ paymentMicroservice/src/main/java/kafka/practice/paymentmicroservice/service/impl/PaymentServiceImpl.java
@@ -0,0 +1,28 @@
+package kafka.practice.paymentmicroservice.service.impl;
+
+import kafka.practice.api.entity.Payment;
+import kafka.practice.paymentmicroservice.repository.PaymentRepository;
+import kafka.practice.paymentmicroservice.service.PaymentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+public class PaymentServiceImpl implements PaymentService {
+
+    private PaymentRepository paymentRepository;
+    private final Logger log = LoggerFactory.getLogger(CreditServiceImpl.class);
+
+    @Autowired
+    public PaymentServiceImpl(PaymentRepository paymentRepository) {
+        this.paymentRepository = paymentRepository;
+    }
+
+    public Mono<Boolean> save(Payment payment) {
+        return paymentRepository.save(payment);
+    }
+
+}
---
+++ paymentMicroservice/src/test/java/kafka/practice/paymentmicroservice/PaymentMicroserviceApplicationTests.java
@@ -0,0 +1,11 @@
+package kafka.practice.paymentmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentMicroserviceApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
