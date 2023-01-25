8dc8a8448e56ae27bb1712a56cbf0d82bca14ca6
GIGABYTE <eo.ursuz@gmail.com>
2023-01-25 18:50:15 +0300

    initial commit

--- borrowerMicroservice/src/main/java/kafka/practice/borrowermicroservice/repository/BorrowerRepository.java
+++
@@ -1,9 +0,0 @@
-package kafka.practice.borrowermicroservice.repository;
-
-import kafka.practice.api.entity.Borrower;
-import reactor.core.publisher.Mono;
-
-public interface BorrowerRepository {
-
-  public Mono<Boolean> save(Borrower borrower);
-}
--- borrowerMicroservice/src/main/java/kafka/practice/borrowermicroservice/repository/CreditRepository.java
+++
@@ -1,9 +0,0 @@
-package kafka.practice.borrowermicroservice.repository;
-
-import kafka.practice.api.entity.Credit;
-import reactor.core.publisher.Mono;
-
-public interface CreditRepository {
-
-  public Mono<Boolean> save(Credit credit);
-}
